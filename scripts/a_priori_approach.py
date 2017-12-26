1cfe9094a4db1752b9ef1a28bb8ed42c7c15e5c1
GiovanniMisseri <giovanni.misseri@gmail.com>
2017-12-26 11:12:52 +0100

    generalization *_fromvids

--- scripts/a_priori_approach.py
+++ scripts/a_priori_approach.py
@@ -15,9 +15,9 @@ surprise=pd.Series([0.15,0.19,0.08,0.76,0.0,0.02,0.0,0.10,0.04,0.0,0.04,0.09,0.2
 neutral=pd.Series([0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
 
-path="../data/labeled_light.csv"  #my path to the file 
+path="../data/labeled_fromvids.csv"  #my path to the file 
 dat=pd.read_csv(path)#opening the file
- 
+
 #form the csv extracting the interest columns
 nam=dat.columns.values
 ok=nam[2:18]
@@ -29,11 +29,21 @@ del opend["AU23_r"]
 
 
 #rescale to [0,1] the dataset
-opend.max()>0
+((opend>0).sum())/327<0.70
 
-opendat=opend.loc[:,opend.max()>0]
+openda=opend.loc[:,((( opend>0).sum())/len(dat.iloc[:,0]) <0.70) ]
 
-dati=opendat/opendat.max()
+opendat=openda.loc[:,openda.max()>0]
+
+
+
+prova=np.array(opendat)
+prova[(prova>3)]=3
+
+
+
+
+dati=prova/prova.max(axis=0)
 a=(dati>1)
 a.sum().sum()
 
@@ -42,14 +52,16 @@ mat=np.zeros((7,14))
 mat[:]=[anger,disgust,fear,happyness,sadness,surprise,neutral]
 matrix=pd.DataFrame(mat, columns=ok )
 
-matrix=matrix.loc[:,opend.max()>0]
+matrix=matrix.loc[:,((opend>0).sum())/len(dat.iloc[:,0]) <0.70]
+matrix=matrix.loc[:,openda.max()>0]
+
 
 #EUCLIDEA
 
 #create a dataframe with the distances computed with the euclidean 
 lis_fin=[]
-for i in range(len(dati.iloc[:,0])):
-    photo=dati.iloc[i,:]
+for i in range(len(dati[:,0])):
+    photo=dati[i,:]
     lis_parz=[]
     for u in range(7):
         emo=matrix.loc[u,:]
@@ -68,39 +80,51 @@ ris.columns=["anger","disgust","fear","happyness","sadness","surprise","neutral"
 temp=[1,3,4,5,6,7,0]
 emotions=pd.DataFrame(temp)
 
+a=ris.iloc[325,:].tolist()
+a.sort()
+c=np.array(temp)
+c[a[0]==ris.iloc[325,:]][0]
 
 #selecting the minimal distance between each photo and the emotions
 clas=[]
 for i in range(len(ris.iloc[:,0])):
-    
-    m=ris.loc[i,:]==min(ris.loc[i,:])
+    a=ris.iloc[i,:].tolist()
+    a.sort()
+    c=np.array(temp)
+    q=c[a[0]==ris.iloc[i,:]][0]
+
+    '''m=ris.loc[i,:]==min(ris.loc[i,:])
     rev=m.tolist()
     rr=emotions[rev][0].tolist()[0]
-    clas.append(rr)
+    clas.append(rr)'''
+    clas.append(q)
 
 papa=pd.Series(clas)
 
 
 #percentage of right clissified emotions
-((dat["emotion"]==papa).sum())/400
+((dat["emotion"]==papa).sum())/327
 
 
 #MINKOWSKI
 
 #like in the euclidean distance, but here we compute the distance with the minkowski degree going from 1 to 201
 lis_ne=[]
-'''for v in range(200):
-    for i in range(len(dati.iloc[:,0])):
-        photo=dati.iloc[i,:]
+for v in range(200):
+    for i in range(len(dati[:,0])):
+        photo=dati[i,:]
         lis_parz=[]
         for u in range(7):
             emo=matrix.loc[u,:]
             dist=scipy.spatial.distance.minkowski(emo,photo,p=v+1)
             lis_parz.append(dist)
             
-        lis_ne.append(lis_parz)   '''
-        
+        lis_ne.append(lis_parz)   
+ 
 
+ 
+      
+'''
 for i in range(len(dati.iloc[:,0])):
     photo=dati.iloc[i,:]
     lis_parz=[]
@@ -109,7 +133,7 @@ for i in range(len(dati.iloc[:,0])):
         dist=scipy.spatial.distance.minkowski(emo,photo,p=1)
         lis_parz.append(dist)
     lis_ne.append(lis_parz)   
-    
+ '''   
 ris_m=pd.DataFrame(lis_ne)        
 
 
@@ -129,7 +153,6 @@ emotions=pd.DataFrame(temp)
 clas=[]
 for i in range(len(ris_m.iloc[:,0])):
     
-    sor=ris_m.loc[i,:].tolist().sort()
     m=ris_m.loc[i,:]==min(ris_m.loc[i,:])
     rev=m.tolist()
     rr=emotions[rev][0].tolist()[0]
@@ -138,14 +161,16 @@ for i in range(len(ris_m.iloc[:,0])):
 papa_m=pd.Series(clas)
 
 
-'''
+
 derr=[]
 for i in range(200):
-    temp=papa_m[i*400:400+i*400]
-    temp.index=range(400)
-    a=(dat["emotion"]==temp).sum() / 400
+    temp=papa_m[i*len(dati[:,0]):len(dati[:,0])+i*len(dati[:,0])]
+    temp.index=range(len(dati[:,0]))
+    a=(dat["emotion"]==temp).sum() / len(dati[:,0])
     derr.append(a)
 trend=pd.Series(derr)
+trend
+
 '''
 ((dat["emotion"]==papa_m).sum())/400
 
@@ -153,7 +178,7 @@ trend=pd.Series(derr)
 #percentage of accurancy usig the different degree
 #print(trend)
 
-
+'''
 
 
 
