db20bbebf8e0e4ed56ceae2c508388098506061b
damnko <yarlaz@gmail.com>
2018-01-07 14:56:33 +0100

    fix: updated dir locations

--- scripts/create_videos.py
+++ scripts/create_videos.py
@@ -2,23 +2,23 @@ import glob
 from shutil import copyfile
 import subprocess
 
-img_folder = "data/cohn-kanade-images"
-lbl_folder = "data/Emotion"
-out_folder = "data/sorted"
+img_folder = "..\\data\\cohn-kanade-images"
+lbl_folder = "..\\data\\Emotion"
+out_folder = "..\\data\\sorted"
 
 emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] #Define emotion order
-participants = glob.glob(img_folder+"/*") #Returns a list of all folders with participant numbers
+participants = glob.glob(img_folder+"\\*") #Returns a list of all folders with participant numbers
 
 for x in participants:
 
     part = "%s" %x[-4:] #store current participant number
     
-    for sessions in glob.glob("%s/*" %x): #Store list of sessions for current participant
+    for sessions in glob.glob("%s\\*" %x): #Store list of sessions for current participant
         print(x)
         print(sessions)
-        vid_name = glob.glob(sessions+"/*")[-1].split("/")[-1]
+        vid_name = glob.glob(sessions+"\\*")[-1].split("\\")[-1]
         print(vid_name)
-        bashCommand = "ffmpeg -start_number 0 -i {}/{}%02d.png -vcodec mpeg4 ./data/ckplus_vids/{}.avi".format(sessions,vid_name[:15],vid_name[:17])
+        bashCommand = "C:\\Users\\dask\\Downloads\\ffmpeg-20171111-3af2bf0-win64-static\\bin\\ffmpeg -start_number 0 -i {}\\{}%02d.png -vcodec mpeg4 C:\\emotion_recognition\\data\\ckplus_vids\\{}.avi".format(sessions,vid_name[:15],vid_name[:17])
 
 
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
--- scripts/extract_images.py
+++ scripts/extract_images.py
@@ -2,9 +2,9 @@ import glob
 from shutil import copyfile
 import subprocess
 
-img_folder = "data\\cohn-kanade-images"
-lbl_folder = "data\\Emotion"
-out_folder = "data\\sorted"
+img_folder = "..\\data\\ckplus_vids"
+lbl_folder = "..\\data\\Emotion"
+out_folder = "..\\data\\sorted"
 
 emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] #Define emotion order
 participants = glob.glob(lbl_folder+"\\*") #Returns a list of all folders with participant numbers
@@ -23,11 +23,11 @@ for x in participants:
             emotion = int(float(file.readline())) #emotions are encoded as a float, readline as float, then convert to integer.
             
             print("%s\\%s\\%s\\*" %(img_folder, part, current_session))
-            sourcefiles = glob.glob("%s\\%s\\%s\\*" %(img_folder, part, current_session)) #get path for last image in sequence, which contains the emotion
-            print(sourcefiles)
+            sourcefiles = glob.glob("%s\\*" %(img_folder)) #get path for last image in sequence, which contains the emotion
+            # print(sourcefiles)
             for img in sourcefiles:
-                
-                bashCommand = "%sFeatureExtraction.exe -f %s -of Out\\%s.txt"%("C:\\Users\\luigimissri\\Desktop\\gianni\\uni\\open_face\\",img,img[-21:-4])
+                print('doing img', img)
+                bashCommand = "%sFeatureExtraction.exe -f C:\\emotion_recognition\\scripts\\%s -of C:\\Users\\dask\\Downloads\\OpenFace_0.2.4_win_x64\\Out\\%s.txt"%("C:\\Users\\dask\\Downloads\\OpenFace_0.2.4_win_x64\\",img,img[-21:-4])
                 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate() 
             
